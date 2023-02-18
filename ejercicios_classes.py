9742d72907c02c0d4d83663d5c7ac4130a94248c
Paolo Soncco <paholo2054@gmail.com>
2023-02-17 22:43:52 -0500

    classes

--- ejercicios_classes.py
+++ ejercicios_classes.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 # Crear una Classe Operaciones con sus respectivos métodos
 # (sumar, restar, multiplicar y dividir). Esta clase recibirá dos números.
 
@@ -24,4 +25,28 @@ class OperacionesMatematicas:
 
 operaciones = OperacionesMatematicas(5, 3)
 
-print(operaciones.dividir())
+# print(operaciones.dividir())
+
+# Crear una clase Usuario que recibar los datos de un usuario
+# (nombre, edad, dni, estatura, estado civil) y convertir estos datos en un diccionario
+
+class Usuario:
+    def __init__(self, nombre, edad, dni, estatura, estado_civil) -> None:
+        self.nombre = nombre
+        self.edad = edad
+        self.dni = dni
+        self.estatura = estatura
+        self.estado_civil = estado_civil
+      
+    def convertirDiccionario(self):
+        return {
+            'nombre': self.nombre,
+            'edad': self.edad,
+            'dni': self.dni,
+            'estatura': self.estatura,
+            'estado_civil': self.estado_civil
+        }
+    
+usuario = Usuario('Paolo', 25, 77337722, 1.80, 'soltero')
+
+pprint(usuario.convertirDiccionario())
