164a2f34f2e7d34a96d1dd82bccfac472aba8756
Paolo Soncco <paholo2054@gmail.com>
2023-02-17 20:05:09 -0500

    ejercicios con arrays

---
+++ ejercicios_funciones.py
@@ -0,0 +1,37 @@
+from pprint import pprint
+# Crear una funcion que reciba la lista de ciudades y ordenarlos
+# por la cantidad de habitantes de menor a mayor
+
+ciudades = [
+    {
+        'nombre': 'Tumbes',
+        'habitantes': 500000
+    },
+    {
+        'nombre': 'Arequipa',
+        'habitantes': 800000
+    },
+    {
+        'nombre': 'Loreto',
+        'habitantes': 10000
+    }
+]
+
+def miFuncion(ciudad):
+    return ciudad['habitantes']
+
+ciudades.sort(key=miFuncion, reverse=True)
+ciudades.append({'nombre': 'Cusco', 'habitantes': 20000})
+# ciudades.pop(0)
+# ciudades.remove({'nombre': 'Cusco', 'habitantes': 20000})
+
+index = 0
+for ciudad in ciudades:
+    if ciudad['nombre'] == 'Cusco':
+        ciudades.remove(ciudades[index])
+    index = index + 1
+pprint(ciudades)
+
+lista = ['Arequipa', 'Cusco', 'Tumbes']
+lista.remove('Arequipa')
+# print(lista)
