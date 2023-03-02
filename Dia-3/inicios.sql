f23ef9c4fcb338d7b4d8509188ca0ad499e9b9af
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2023-03-01 19:17:50 -0500

    resolucion de la tarea

--- Dia-3/inicios.sql
+++ Dia-3/inicios.sql
@@ -105,11 +105,22 @@ CREATE TABLE direcciones(
 
 
 -- 1. Buscar todas las direcciones que sean calles
+SELECT * FROM direcciones WHERE direccion LIKE '%Calle %';
+
 -- 2. Listar todas las direcciones sin referencia
+SELECT * FROM direcciones WHERE referencia IS NULL;
+
 -- 3. Listar todas las direcciones que sean menores que 1000
+SELECT * FROM direcciones WHERE numero < 1000;
+
 -- 4. Listar todas las direcciones que sean o Av o Pasaje
+SELECT * FROM direcciones WHERE direccion LIKE 'Av%' OR direccion LIKE 'Pasaje%';
+
 -- 5. Listar todas las direcciones de los alumnos 1 o que vivan en calles o que no tengan referencias
+SELECT * FROM direcciones WHERE alumno_id = 1 OR direccion LIKE 'Calle %' OR referencia IS NULL;
+
 -- 6. Listar todas las direcciones que sean calle y que su referencia no sea nula y que su alumno sea el 1
+SELECT * FROM direcciones WHERE direccion LIKE 'Calle %' AND referencia NOT IS NULL AND alumno_id = 1;
 
 
 -- Para borrar una tabla
