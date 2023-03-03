784f3aa96caf48c90f7702ed554d54fe7767781e
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2023-03-02 19:45:47 -0500

    avance uso de bd con flask

--- Dia-1/flask-postgres.py
+++ Dia-1/flask-postgres.py
@@ -2,6 +2,8 @@ from flask import Flask, request
 from psycopg2 import connect
 
 app = Flask(__name__)
+# me conecto a la bd
+conexion = connect(host='localhost', database='pruebas', user='postgres', password='root')
 
 @app.route('/', methods = ['GET'])
 def inicial():
@@ -12,9 +14,6 @@ def inicial():
 @app.route('/alumnos', methods = ['GET', 'POST'])
 def alumnos():
     if request.method == 'GET':
-        # me conecto a la bd
-        conexion = connect(host='localhost', database='pruebas', user='postgres', password='root')
-        
         # creo un cursor que es el responsable de hacer lecturas y escrituras a la bd
         cursor = conexion.cursor()
         
@@ -25,11 +24,31 @@ def alumnos():
         resultado = cursor.fetchall()
 
         print(resultado)
+        alumnos_resultado = []
+
+        for alumno in resultado:
+            info_alumno = {
+                'id': alumno[0],
+                'nombre': alumno[1],
+                'apellido': alumno[2],
+                'sexo': alumno[3],
+                'fecha_creacion': alumno[4],
+                'matriculado': alumno[5]
+            }
+
+            alumnos_resultado.append(info_alumno)
         return {
-            'message': 'Yo soy el GET'
+            'message': alumnos_resultado
         }
     
     elif request.method == 'POST':
+        data = request.json
+        print(data)
+        cursor = conexion.cursor()
+        cursor.execute("INSERT INTO ALUMNOS (nombre, apellido, matriculado) VALUES (%s, %s, %s)",(
+            data.get('nombre'), data.get('apellido'), data.get('matriculado')
+        ))
+
         return {
             'message': 'Yo soy el POST'
         }
