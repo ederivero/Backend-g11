bf0b04f42f45b7b5343f3de4479bff0c42f17e16
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2023-03-02 23:04:00 -0500

    creacion del primer modelo y modelado

--- flask-con-orm/app.py
+++ flask-con-orm/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from base_de_datos import conexion
 from dotenv import load_dotenv
 from os import environ
 
@@ -8,7 +8,11 @@ load_dotenv() # es en el encargado de leer el archivo .env si es que existe y ag
 app = Flask(__name__)
 # dialecto://usuario:password@host:puerto/base_de_datos
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
-conexion = SQLAlchemy(app=app)
+
+# si quiero crear mi conexion en otro archivo e inicializar la configuracion de mi conexion tengo que utilizar el metodo init_app y es aca donde le pasare el parametro de mi instancia de la clase Flask
+conexion.init_app(app)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ flask-con-orm/models/nivel_model.py
@@ -0,0 +1,11 @@
+from base_de_datos import conexion
+from sqlalchemy import Column, types
+
+# Las tablas se van a comportar como si fueran clases y cada columna sera un atributo de la clase
+class Nivel(conexion.Model):
+    id = Column(type_=types.Integer, primary_key=True, autoincrement=True)
+    numero = Column(type_=types.Integer, nullable=False, unique=True)
+    descripcion = Column(type_=types.Text)
+
+    # como se llamara la tabla en la base de datos
+    __tablename__ = 'niveles'
