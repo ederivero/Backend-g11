c11773710f7de8b9c6db2cadff42ed3995844b8c
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2023-03-29 23:02:08 -0500

    creacion de tablas con postgres

--- veterinaria/veterinaria/settings.py
+++ veterinaria/veterinaria/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from os import environ
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +81,11 @@ WSGI_APPLICATION = 'veterinaria.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': environ.get('NOMBRE_DB'),
+        'USER': environ.get('USER_DB'),
+        'PASSWORD': environ.get('PASSWORD_DB'),
+        'PORT': environ.get('PORT_DB')
     }
 }
 
