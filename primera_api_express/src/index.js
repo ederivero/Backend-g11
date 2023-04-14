c819f44680e80345ceb872c9017a5cdfcfb0602c
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2023-04-13 20:18:15 -0500

    uniendo controladores

--- primera_api_express/src/index.js
+++ primera_api_express/src/index.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const servidor = express();
 
+// sirve para indicar un middleware > es un controlador que va a interactuar con todas las peticiones que ingresen a mi API
+// json() > sirve para indicar que mi aplicacion podra 'entender' los JSON entrantes por el body y lo convertira a un json legible
+servidor.use(express.json());
+
 const productos = [
   {
     nombre: "Martillo",
@@ -19,7 +23,35 @@ const productos = [
     disponible: false,
   },
 ];
+
 const PORT = 3000;
+
+servidor
+  .route("/productos")
+  .get((req, res) => {
+    res.status(200).json({
+      content: productos,
+    });
+  })
+  .post((req, res) => {
+    console.log(req.body);
+    // convierto el JSON a un string y por ende si el valor es '{}' significa que esta vacio
+    //   if (JSON.stringify(req.body) === "{}") {
+    // { nombre : 'Eduardo' } > Object.keys({...}) > ['nombre', '...']
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+    if (Object.keys(req.body).length === 0) {
+      res.status(400).json({
+        message: "Informacion incorrecta",
+      });
+    }
+
+    productos.push(req.body);
+
+    res.status(201).json({
+      message: "Producto agregado exitosamente",
+      content: req.body,
+    });
+  });
 
 servidor.listen(PORT, () => {
   console.log(`Servidor corriendo exitosamente en el puerto ${PORT}`);
