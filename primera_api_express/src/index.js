8fa23495f51d6284451065f9ce9af07546ab494f
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2023-04-13 21:39:50 -0500

    uso de regex y busquedas

--- primera_api_express/src/index.js
+++ primera_api_express/src/index.js
@@ -53,6 +53,89 @@ servidor
     });
   });
 
+servidor
+  .route("/producto/:id")
+  .get((req, res) => {
+    // en base al id que sera la posicion del arreglo devolver el producto
+    const { id } = req.params;
+    const resultado = productos[id];
+
+    if (resultado) {
+      //   const id = req.params.id;
+      res.status(200).json({
+        content: resultado,
+      });
+    } else {
+      res.status(404).json({
+        message: "El producto no existe",
+      });
+    }
+  })
+  .put((req, res) => {
+    const { id } = req.params;
+    const body = req.body;
+
+    const resultado = productos[id];
+
+    if (!resultado) {
+      res.status(404).json({
+        message: "El producto no existe",
+      });
+    }
+
+    productos[id] = body;
+
+    res.status(201).json({
+      message: "Producto actualizado exitosamente",
+      content: productos[id],
+    });
+  })
+  .patch((req, res) => {
+    // TODO: Hacer la actualizacion parcial Por ejemplo si solo quiero cambiar el nombre o si solo quiero cambiar el nombre y el precio
+  })
+  .delete((req, res) => {
+    const { id } = req.params;
+    const resultado = productos[id];
+
+    if (!resultado) {
+      res.status(404).json({
+        message: "El producto no existe",
+      });
+    }
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/pop
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/shift
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+    productos.splice(id, 1);
+
+    res.status(200).json({
+      message: "Producto eliminado exitosamente",
+    });
+  });
+
+servidor.route("/buscar-productos").get((req, res) => {
+  console.log(req.query);
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+  let resultado = [];
+
+  if (req.query.nombre) {
+    resultado = productos.filter((producto) => {
+      // si en la posicion que estamos en el arreglo retorna true entonces se agregara ese elemento al nuevo arreglo
+      return producto.nombre === req.query.nombre;
+    });
+  }
+
+  // Para que sea la busqueda de un inicio de una palabra mediante expresiones regulares
+  if (req.query.patron) {
+    resultado = productos.filter((producto) => {
+      // podemos utilizar expresiones regulares para hacer busqueda de patrones
+      return new RegExp(`(${req.query.patron})\w*`).test(producto.nombre);
+    });
+  }
+  res.status(200).json({
+    content: resultado,
+  });
+});
+
 servidor.listen(PORT, () => {
   console.log(`Servidor corriendo exitosamente en el puerto ${PORT}`);
 });
